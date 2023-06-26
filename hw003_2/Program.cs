93b429d601c26a39ab6f81c6c66c5462f6239885
alexnif <alexnif@mail.ru>
2023-06-26 15:13:19 +0700

    add 3 task

---
+++ hw003_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Задание: Напишите программу, которая на вход принимает число и выдаёт,является ли число чётным(делится ли оно на два без остатка).");
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+Console.WriteLine("Число четное");
+else 
+ Console.WriteLine("Число не четное");
+
+
