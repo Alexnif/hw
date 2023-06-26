0cc951c2b560bf7881d8cf94d4e2c5682c05e554
alexnif <alexnif@mail.ru>
2023-06-26 13:17:51 +0700

    add 1st task

--- hw003/Program.cs
+++ hw003/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Вводим два числа - выводим, какое число большее, а какое меньшее");
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"Первое число {a}  больше второго {b}.");
+}
+else if (a != b)
+{
+    Console.WriteLine($"Первое число {a}  меньше второго {b}.");
+}
+else
+{
+    Console.WriteLine($"Числа {a} и {b} равны");
+}
