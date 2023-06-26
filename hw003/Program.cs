03382c91de196e56d1bf6c4cff8a1c9a9e11d50e
alexnif <alexnif@mail.ru>
2023-06-26 14:54:55 +0700

    add 2 task

--- hw003/Program.cs
+++ hw003/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Вводим два числа - выводим, какое число большее, а какое меньшее");
+Console.WriteLine("Задание: Ввести два числа. Вывести какое число большее, а какое меньшее");
 Console.WriteLine("Введите первое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.Read());
 Console.WriteLine("Введите второе число: ");
-int b = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.Read());
 if (a > b)
 {
     Console.WriteLine($"Первое число {a}  больше второго {b}.");
